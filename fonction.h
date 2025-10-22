10697c20437c7ebb6549446ef85b093f748f5a47
amira <amiraothmani920@gmail.com>
2025-10-22 23:26:30 +0100

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,3 +1,19 @@
 
 
-#include
+#include <gtk/gtk.h>
+typedef struct{
+   Gtkwidget *AOgestcours;
+   Gtkwidget *AOnotebook;
+   //Pilates
+Gtkwidget *AOgPilates;
+//Labels
+Gtkwidget *AO_int1;
+Gtkwidget *AOlbl_description1;
+Gtkwidget *AO_Des1;
+Gtkwidget *AO_niveau1;
+Gtkwidget *AOjour1;
+Gtkwidget *AOduree1;
+Gtkwidget *AOsalleP;
+
+   
+
