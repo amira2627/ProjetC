b04d742d2fabdc339ddb557ac4091caf3d7c3656
amira <amiraothmani920@gmail.com>
2025-10-23 00:21:53 +0100

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,19 +1,138 @@
-
-
-#include <gtk/gtk.h>
+#ifndef fonction
+#define fonction
 typedef struct{
-   Gtkwidget *AOgestcours;
-   Gtkwidget *AOnotebook;
-   //Pilates
-Gtkwidget *AOgPilates;
-//Labels
-Gtkwidget *AO_int1;
-Gtkwidget *AOlbl_description1;
-Gtkwidget *AO_Des1;
-Gtkwidget *AO_niveau1;
-Gtkwidget *AOjour1;
-Gtkwidget *AOduree1;
-Gtkwidget *AOsalleP;
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurPilates[20];
+   char jour[50];
+   char jourP[50];
+   int duree;
+   char salle[10];
+   char salleP[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Pilates;
+typedef struct{
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurYoga[20];
+   char jour[50];
+   char joury[50];
+   int duree;
+   char salle[10];
+   char salleY[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Yoga;
+typedef struct{
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurDance[20];
+   char jour[50];
+   char jourD[50];
+   int duree;
+   char salle[10];
+   char salleD[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Dance;
+typedef struct{
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurMusculation[20];
+   char jour[50];
+   char jourM[50];
+   int duree;
+   char salle[10];
+   char salleM[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Musculation;
+typedef struct{
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurBoxe[20];
+   char jour[50];
+   char jourB[50];
+   int duree;
+   char salle[10];
+   char salleB[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Boxe;
+typedef struct{
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurKarate[20];
+   char jour[50];
+   char joury[50];
+   int duree;
+   char salle[10];
+   char salleK[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Karate;
+typedef struct{
+   char description[100];
+   int debutant;
+   int intermediaire;
+   int avance;
+   char entraineur[20];
+   char entraineurCardio[20];
+   char jour[50];
+   char jourC[50];
+   int duree;
+   char salle[10];
+   char salleC[20];
+   int mensuel;
+   int annuel;
+   int renouvelerabonnement;
+   int payerparseance;
+   int payerparseance;
+}Cardio;
+
+
+
+
 
    
+   
+
+
 
