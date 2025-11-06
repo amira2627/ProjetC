f2a0e39f7600fbc1f9321d4eb5eaf5d3f60b1260
amira <amiraothmani920@gmail.com>
2025-11-06 11:33:40 +0100

    Update Cours.h

--- fonction.h
+++ fonction.h
@@ -1,141 +1,36 @@
 #ifndef fonction
 #define fonction
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurPilates[20];
-   char jour[50];
-   char jourP[50];
-   int duree;
-   char salle[10];
-   char salleP[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Pilates;
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurYoga[20];
-   char jour[50];
-   char joury[50];
-   int duree;
-   char salle[10];
-   char salleY[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Yoga;
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurDance[20];
-   char jour[50];
-   char jourD[50];
-   int duree;
-   char salle[10];
-   char salleD[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Dance;
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurMusculation[20];
-   char jour[50];
-   char jourM[50];
-   int duree;
-   char salle[10];
-   char salleM[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Musculation;
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurBoxe[20];
-   char jour[50];
-   char jourB[50];
-   int duree;
-   char salle[10];
-   char salleB[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Boxe;
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurKarate[20];
-   char jour[50];
-   char joury[50];
-   int duree;
-   char salle[10];
-   char salleK[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Karate;
-typedef struct{
-   char description[100];
-   int debutant;
-   int intermediaire;
-   int avance;
-   char entraineur[20];
-   char entraineurCardio[20];
-   char jour[50];
-   char jourC[50];
-   int duree;
-   char salle[10];
-   char salleC[20];
-   int mensuel;
-   int annuel;
-   int renouvelerabonnement;
-   int payerparseance;
-   int payerparseance;
-}Cardio;
-void sinscrire();
-void annulerinscription();
-#endif
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char type[50];                   // Pilates, Yoga, Dance, Musculation, Boxe, Karate, Cardio
+    char description[100];           // Description du cours
+    int niveau;                      // 0=Débutant, 1=Intermédiaire, 2=Avancé
+    char entraineur[50];             // Nom de l'entraineur
+    char jour[20];                   // Jour du cours (Mardi, Jeudi, etc.)
+    char horaire[10];                // Horaire du cours (9h30, 19h15, etc.)
+    int duree;                       // Durée en minutes
+    char salle[20];                  // Nom de la salle
+    int type_abonnement;             // 0=Mensuel, 1=Annuel, 2=Payer par séance
+    int renouveler_abonnement;       // 0=Non, 1=Oui
+} Cours;typedef struct {
+
+    
+} Cours_admin;
 
 
 
-   
-   
+int ajouter_cours(char *, Cours_admin );
+int modifier_cours(char *, int , Cours_admin);
+int supprimer_cours(char *, int);
+Cours chercher_cours(char *, int);
+
+// Prototypes des fonctions d'interface
+int sinscrire(char *, Cours);
+int annuler_inscription(char *, int );
 
 
 
-
-
+#endif 
